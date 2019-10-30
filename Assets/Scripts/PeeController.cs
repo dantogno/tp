2d4ba63463185054d3e829f298111d8ccd31e0c2
David Antognoli <david.antognoli@gmail.com>
2019-10-30 14:05:45 -0500

    Pee emitter moves around with the player. Now I need to adjust it's rotation.

--- Assets/Scripts/PeeController.cs
+++ Assets/Scripts/PeeController.cs
@@ -6,15 +6,21 @@ public class PeeController : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem particleSystem;
+
+    [SerializeField]
+    private Transform objectToFollow;
+
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - objectToFollow.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = objectToFollow.position + offset;
         if (Input.GetButton("Pee"))
         {
             Debug.Log("Pee input pressed");
