c1cae54a55f3bceb8157f2c227b09c497aba7814
David Antognoli <david.antognoli@gmail.com>
2019-10-30 12:22:45 -0500

    Pee effect triggered with button press

---
+++ Assets/Scripts/PeeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeeController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Pee"))
+        {
+            Debug.Log("Pee input pressed");
+            if (!particleSystem.isPlaying)
+                particleSystem.Play();
+        }
+        if (Input.GetButtonUp("Pee"))
+        {
+            particleSystem.Stop();
+        }
+    }
+}
