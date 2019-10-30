c6fe391b5021dedc7f3aa8f06b2efdd3e225de0d
David Antognoli <david.antognoli@gmail.com>
2019-10-30 15:21:15 -0500

    pee follows player, rotates with player based on a bounding box

--- Assets/Scripts/PeeController.cs
+++ Assets/Scripts/PeeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,12 @@ public class PeeController : MonoBehaviour
 
     [SerializeField]
     private Transform objectToFollow;
+
+    [SerializeField]
+    private float rotationThreshold;
+
+    [SerializeField]
+    private float rotationSpeed = 1.0f;
 
     private Vector3 offset;
     // Start is called before the first frame update
@@ -21,15 +28,46 @@ public class PeeController : MonoBehaviour
     void Update()
     {
         transform.position = objectToFollow.position + offset;
+
         if (Input.GetButton("Pee"))
         {
-            Debug.Log("Pee input pressed");
             if (!particleSystem.isPlaying)
+            {
                 particleSystem.Play();
+                SetInitialPeeingRotation();
+            }
+            else
+                UpdateRotationWhilePeeing();       
         }
         if (Input.GetButtonUp("Pee"))
         {
             particleSystem.Stop();
+        }
+    }
+
+    /// <summary>
+    /// When the player first starts peeing, snap the pee rotation Y aim to where the player is looking.
+    /// </summary>
+    private void SetInitialPeeingRotation()
+    {
+        var newRotation = Quaternion.LookRotation(objectToFollow.localEulerAngles);
+        newRotation.eulerAngles = new Vector3(0, objectToFollow.localEulerAngles.y, 0);
+        transform.rotation = newRotation;
+    }
+
+    /// <summary>
+    /// If the difference between the player's Y rotation and the paricle
+    /// system's Y rotation is greater than the threshold, then the
+    /// particle system should begin to rotate along with the player.
+    /// </summary>
+    private void UpdateRotationWhilePeeing()
+    {
+        if (Mathf.Abs(transform.eulerAngles.y - objectToFollow.eulerAngles.y) > rotationThreshold)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            var newRotation = Quaternion.RotateTowards(transform.rotation, objectToFollow.localRotation, step);
+            newRotation.eulerAngles = new Vector3(0, newRotation.eulerAngles.y, 0);
+            transform.rotation = newRotation;
         }
     }
 }
