8683846baf466b287486f6644ad230c8feae067d
David Antognoli <david.antognoli@gmail.com>
2019-11-04 12:19:23 -0600

    added a player body collider for trigger based interactions

---
+++ Assets/Scripts/PlayerColliderForTriggers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColliderForTriggers : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objectToFollow;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - objectToFollow.position;
+    }
+    private void Update()
+    {
+        transform.position = objectToFollow.position + offset;
+    }
+}
