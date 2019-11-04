f9dda9c891cc35b017f0fbea219b32c782901962
David Antognoli <david.antognoli@gmail.com>
2019-11-04 15:40:32 -0600

    Got it to the point where I can press a button and make the seat fall.
    Seems to take greater force in VR vs editor... not sure why.
    I had to write a script that enables gravity and adds a little push to get it to fall. Then I have to disable gravity when it gets a certain way closed, or it will fall too fast and go past the limit of the joint. Also, if you have to disable gravity on the rigidbody again, or it will become jittery.

--- Assets/Scripts/ToiletLidPusher.cs
+++ Assets/Scripts/ToiletLidPusher.cs
@@ -10,12 +10,39 @@ public class ToiletLidPusher : MonoBehaviour
     [SerializeField]
     private float forceMultiplier = 2.0f;
 
+    [SerializeField]
+    private float closedRotationEulerAngles = 83.0f;
+
+    [SerializeField]
+    private float closedThreshold = 0.1f;
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("PeeButton"))
         {
+            toiletLidRigidbody.useGravity = true;
             toiletLidRigidbody.AddForce(toiletLidRigidbody.transform.forward * forceMultiplier);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (toiletLidRigidbody.useGravity)
+        {
+            if (toiletLidRigidbody.transform.localEulerAngles.x > closedRotationEulerAngles - closedThreshold && 
+                toiletLidRigidbody.transform.localEulerAngles.x < closedRotationEulerAngles + closedThreshold)
+            {
+                Debug.Log("PositionReached");
+                toiletLidRigidbody.useGravity = false;                
+            }
+            else
+                Debug.Log(toiletLidRigidbody.transform.localEulerAngles.x);
+        }
+    }
+
+    private void Start()
+    {
+        
     }
 
 }
