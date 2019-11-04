541d31be1378c7a9622a7d39900e353d90316ebb
David Antognoli <david.antognoli@gmail.com>
2019-11-04 14:11:40 -0600

    revised limits of rotational drive prefab for toilet lid. testing pushing lid shut.

---
+++ Assets/Scripts/ToiletLidPusher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletLidPusher : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody toiletLidRigidbody;
+
+    [SerializeField]
+    private float forceMultiplier = 2.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            toiletLidRigidbody.AddForce(toiletLidRigidbody.transform.forward * forceMultiplier);
+        }
+    }
+
+}
