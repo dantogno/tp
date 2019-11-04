c607eec31d7c54ec254c9732eb6c9511b1fdbbd9
David Antognoli <david.antognoli@gmail.com>
2019-11-04 16:57:49 -0600

    Player entering trigger region is working.

---
+++ Assets/Scripts/CloseLidOnTriggerEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseLidOnTriggerEnter : MonoBehaviour
+{
+    private ToiletLidPusher toiletLidPusher;
+    private void Awake()
+    {
+        toiletLidPusher = FindObjectOfType<ToiletLidPusher>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            toiletLidPusher.PushLidClosed();
+            Debug.Log("Entered trigger!");
+        }
+    }
+}
--- Assets/Scripts/ToiletLidPusher.cs
+++ Assets/Scripts/ToiletLidPusher.cs
@@ -20,29 +20,34 @@ public class ToiletLidPusher : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("PeeButton"))
         {
-            toiletLidRigidbody.useGravity = true;
-            toiletLidRigidbody.AddForce(toiletLidRigidbody.transform.forward * forceMultiplier);
+            PushLidClosed();
         }
     }
 
     private void FixedUpdate()
     {
+        // useGravity helps the lid fall more naturally, but it must be disabled when the lid closes
+        // to avoid jittery behavior. I'm turing it off early to help prevent it from closing too far,
+        // beyond the limit of the RotationalJointDrive component, which can happen if it gathers too much speed.
         if (toiletLidRigidbody.useGravity)
         {
             if (toiletLidRigidbody.transform.localEulerAngles.x > closedRotationEulerAngles - closedThreshold && 
                 toiletLidRigidbody.transform.localEulerAngles.x < closedRotationEulerAngles + closedThreshold)
             {
-                Debug.Log("PositionReached");
+                // Debug.Log("PositionReached");
                 toiletLidRigidbody.useGravity = false;                
             }
             else
-                Debug.Log(toiletLidRigidbody.transform.localEulerAngles.x);
+            {
+                //Debug.Log(toiletLidRigidbody.transform.localEulerAngles.x);
+            }
         }
     }
 
-    private void Start()
+    public void PushLidClosed()
     {
-        
+        toiletLidRigidbody.useGravity = true;
+        toiletLidRigidbody.AddForce(toiletLidRigidbody.transform.forward * forceMultiplier);
     }
 
 }
