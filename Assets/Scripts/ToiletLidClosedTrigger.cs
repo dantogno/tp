b6baba2566f8df079d56445ab864de427359d291
David Antognoli <david.antognoli@gmail.com>
2019-11-04 17:39:38 -0600

    toilet open and close triggers

---
+++ Assets/Scripts/ToiletLidClosedTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletLidClosedTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ToiletClosed");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Started opening toilet lid!");   
+    }
+}
---
+++ Assets/Scripts/ToiletLidOpenTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletLidOpenTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Toilet lid is fully open.");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Started closing toilet lid!");
+    }
+}
