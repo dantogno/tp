828af82c2968506340ca090eaa4bcb6ddb2d91e9
David Antognoli <david.antognoli@gmail.com>
2019-11-05 10:39:26 -0600

    Flush lever can move appropriately with player interaction
    This was a pain in the ass. When the drive is running, it's too powerful for the player to use the lever. So I have to turn it off and on using the grab / release events.
    
    Then I was also getting horrible jittering while the drive tried to hit the target value. I had to find and increase the target threshold, which was buried somewhere in the VRTK prefab.

---
+++ Assets/Scripts/DisableDriveWhileGrabbed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Interactions.Controllables;
+
+public class DisableDriveWhileGrabbed : MonoBehaviour
+{
+    [SerializeField]
+    private RotationalDriveFacade rotationalDriveFacade;
+
+    public void DisableDriveOnGrab()
+    {
+        rotationalDriveFacade.MoveToTargetValue = false;
+    }
+    public void EnableDriveOnRelease()
+    {
+        rotationalDriveFacade.MoveToTargetValue = true;
+    }
+}
