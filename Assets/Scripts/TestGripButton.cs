6259cc39de82092f3c348439c73fdd4b2503e9cd
David Antognoli <david.antognoli@gmail.com>
2019-11-10 18:07:08 -0600

    import oculus plugin and add avatar hands.

---
+++ Assets/Scripts/TestGripButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGripButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToDisable;
+    public void TestButton()
+    {
+        objectToDisable.SetActive(false);
+    }
+}
