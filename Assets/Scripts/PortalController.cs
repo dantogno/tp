71029897d8d1db69fc070e824b0aa6b1172feabd
David Antognoli <david.antognoli@gmail.com>
2019-11-11 21:18:51 -0600

    tweaking portal entrance fading, adding note

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -22,10 +22,41 @@ public class PortalController : MonoBehaviour
     [SerializeField]
     private GameObject lookAtHitBox;
 
-    private bool isPortalSpawned;
+    [SerializeField]
+    private List<Color> portalColors;
+
+    [SerializeField]
+    private List<MeshRenderer> portalMeshes;
+
+    [SerializeField]
+    private Light portalLight;
+
+    public static int DimensionIndex
+    {
+        get
+        {
+            // put some logic here so it doesn't go out of bounds...
+            return dimensionIndex;
+        }
+    }
+    private static int dimensionIndex;
+    private int colorIndex = 0;
+    private bool isPortalSpawned = false;
     private bool isDemonFlushed;
     private bool isGateShut;
+    private bool obtainedNote;
     private RaycastHit raycastHit;
+    private List<Material> portalMaterials = new List<Material>();
+
+    private void Awake()
+    {
+        foreach (var item in portalMeshes)
+        {
+            portalMaterials.Add(item.material);
+            Debug.Log($"Material name: {item.material.name}");
+        }
+        portalColors.Add(portalLight.color);
+    }
 
     private void FixedUpdate()
     {
@@ -42,6 +73,21 @@ public class PortalController : MonoBehaviour
             }
         }
     }
+    private void OnToiletFlushed()
+    {
+        // Change dimensions if portal is spawned
+        if (isPortalSpawned)
+        {
+            colorIndex++;
+            colorIndex = colorIndex >= portalColors.Count ? 0 : colorIndex;
+
+            portalLight.color = portalColors[colorIndex];
+            foreach (var item in portalMaterials)
+            {
+                item.SetColor("_TintColor", portalColors[colorIndex]);
+            }
+        }
+    }
     private void OnDemonFlushed()
     {
         isDemonFlushed = true;
@@ -52,11 +98,13 @@ public class PortalController : MonoBehaviour
     }
     private void OnEnable()
     {
+        FlushController.ToiletFlushed += OnToiletFlushed;
         GateController.GateShut += OnGateShut;
         FlushDemon.DemonFlushed += OnDemonFlushed;
     }
     private void OnDisable()
     {
+        FlushController.ToiletFlushed -= OnToiletFlushed;
         GateController.GateShut -= OnGateShut;
         FlushDemon.DemonFlushed -= OnDemonFlushed;
     }
