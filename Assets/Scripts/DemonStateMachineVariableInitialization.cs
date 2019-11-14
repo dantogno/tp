c001f8d5147100d5caf800a99c1712dc682ef885
David Antognoli <david.antognoli@gmail.com>
2019-11-13 21:29:52 -0600

    added demon attack sounds

---
+++ Assets/Scripts/DemonStateMachineVariableInitialization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonStateMachineVariableInitialization : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var animator = GetComponent<Animator>();
+        var audioSource = GetComponent<AudioSource>();
+        var audioClipSMBs = animator.GetBehaviours<PlayAudioClipSMB>();
+        foreach (var item in audioClipSMBs)
+        {
+            item.Audio = audioSource;
+        }
+    }
+}
