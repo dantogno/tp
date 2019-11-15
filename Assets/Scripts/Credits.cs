6749eb81c83e2321fc6ecbea69ac8f3f837e29c9
David Antognoli <david.antognoli@gmail.com>
2019-11-15 09:55:31 -0600

    Credits scene and animation added.
    I need to make it a linear speed!

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    private Transform playerHeadTransform;
+
+    private void Start()
+    {
+        // Adjust Y to match player height
+        GameObject playerHeadGameObject = GameObject.Find("HeadsetAlias");
+        Vector3 newPostion = transform.position;
+        newPostion.y = playerHeadGameObject.transform.position.y;
+        transform.position = newPostion;
+    }
+}
