f052c882772ea10b8e0d8ac0ecfae1ce88c8078b
David Antognoli <david.antognoli@gmail.com>
2019-11-16 14:03:49 -0600

    tutorial scene added

---
+++ Assets/Scripts/PeeTargetToLoadScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zinnia.Visual;
+
+public class PeeTargetToLoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private int numberOfParticlesToTriggerLoad = 45;
+
+    [SerializeField]
+    private List<GameObject> objectsToStopBeforeLoad;
+
+    [SerializeField]
+    private AudioSource stingerToPlayOnLoad;
+
+    private int particlesSoFar = 0;
+
+    private CameraColorOverlay cameraColorOverlay;
+    private void Start()
+    {
+        cameraColorOverlay = GetComponent<CameraColorOverlay>();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        particlesSoFar++;
+
+        if (particlesSoFar > numberOfParticlesToTriggerLoad)
+        {
+            cameraColorOverlay.AddColorOverlay();
+            foreach (var item in objectsToStopBeforeLoad)
+            {
+                item.SetActive(false);
+            }
+            stingerToPlayOnLoad.Play();
+            StartCoroutine(LoadCoroutine());
+        }
+    }
+
+    private IEnumerator LoadCoroutine()
+    {
+        yield return new WaitForSeconds(cameraColorOverlay.AddDuration);
+        SceneManager.LoadScene("small_bathroom");
+    }
+}
