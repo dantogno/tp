c43361d93bd4f98bfb34a0ff667aa8fa2af4ee01
David Antognoli <david.antognoli@gmail.com>
2019-11-16 14:16:58 -0600

    portal ambience added

---
+++ Assets/Scripts/AllNonEarthDimensionObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllNonEarthDimensionObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToControl;
+
+    private void OnPortalEntered(DimensionType dimension)
+    {
+        objectToControl.SetActive(true);   
+    }
+    private void OnPortalExited(DimensionType dimension)
+    {
+        objectToControl.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        PortalInnerWorldTrigger.PortalEntered += OnPortalEntered;
+        PortalInnerWorldTrigger.PortalExited += OnPortalExited;
+    }
+    private void OnDisable()
+    {
+        PortalInnerWorldTrigger.PortalEntered -= OnPortalEntered;
+        PortalInnerWorldTrigger.PortalExited -= OnPortalExited;
+    }
+}
