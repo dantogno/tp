61918e5f35b650bee4d62f72ab3ca003f31aa55c
David Antognoli <david.antognoli@gmail.com>
2019-11-16 14:27:12 -0600

    added pee portal sign

---
+++ Assets/Scripts/PeePortalSignController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeePortalSignController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject peeSign, portalSign;
+
+    private void OnGateShut()
+    {
+        peeSign.SetActive(false);
+        portalSign.SetActive(true);
+    }
+    private void OnEnable()
+    {
+        GateController.GateShut += OnGateShut;
+    }
+    private void OnDisable()
+    {
+        GateController.GateShut -= OnGateShut;
+    }
+}
