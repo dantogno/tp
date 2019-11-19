23ea18630edd8dbe328329423a6a9012549e0279
David Antognoli <david.antognoli@gmail.com>
2019-11-19 11:26:10 -0600

    fixed jump scare dimension demon bug where it continuously rises

--- Assets/Scripts/JumpScareDimensionDemon.cs
+++ Assets/Scripts/JumpScareDimensionDemon.cs
@@ -7,31 +7,9 @@ public class JumpScareDimensionDemon : MonoBehaviour
     [SerializeField]
     private AudioSource jumpScareAudio;
 
-    [SerializeField]
-    private Animator demonAnimator;
-
-    [SerializeField]
-    private float jumpScareAudioDelay = 0.15f;
-
-    [SerializeField]
-    private Rigidbody demonPhysicsTransform;
-
-    private int summonAnimTrigger = Animator.StringToHash("Summon");
-    private Vector3 originalPosition;
-    private void Awake()
-    {
-        demonPhysicsTransform.GetComponent<Rigidbody>();
-    }
+ 
     private void OnEnable()
     {
-        originalPosition = demonPhysicsTransform.position;
-        demonAnimator.SetTrigger(summonAnimTrigger);
-        jumpScareAudio.PlayDelayed(jumpScareAudioDelay);
-    }
-    private void OnDisable()
-    {
-        demonAnimator.SetTrigger(summonAnimTrigger);
-        demonPhysicsTransform.position = originalPosition;
-        demonPhysicsTransform.isKinematic = true;
+        jumpScareAudio.Play();
     }
 }
