eeb3a762772028d8eaf65ad920e61c77dc58511c
David Antognoli <david.antognoli@gmail.com>
2019-12-14 17:37:41 -0600

    hand dryer functionality, moved pokers forward and turned off poker renderers

---
+++ Assets/Scripts/HandDryer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandDryer : MonoBehaviour
+{
+    [SerializeField]
+    private float triggerValue = 0.4f;
+
+    [SerializeField]
+    private float initialDisableTime = 1;
+
+    private float timeObjectStarted;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timeObjectStarted = Time.time;
+    }
+
+    public void PlayAudioOnValueChanged(float value)
+    {
+        // Debug.Log($"Value: {value}");
+        if (value < triggerValue && !audioSource.isPlaying && Time.time - timeObjectStarted > initialDisableTime)
+            audioSource.Play();
+    }
+}
