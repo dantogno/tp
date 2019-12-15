f0eb14e00410e63ca386f2894596ce12f53d725e
David Antognoli <david.antognoli@gmail.com>
2019-12-14 18:42:48 -0600

    toilet valve can be pulled off and goop sprays out

---
+++ Assets/Scripts/ToiletValve.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletValve : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particleSystem;
+
+    private float timeObjectStarted;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        particleSystem.Stop();
+    }
+    public void OnGrabbed()
+    {
+        if (!particleSystem.isPlaying)
+            particleSystem.Play();
+    }
+}
